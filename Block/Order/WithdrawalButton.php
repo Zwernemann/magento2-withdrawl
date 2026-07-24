345546f4a95b51dcd8227ced5d0c4f1112cbbca0
Martin Zwernemann <52236002+Zwernemann@users.noreply.github.com>
2026-07-24 13:51:00 +0200

    Optimize withdrawal check with caching
    Refactor hasWithdrawal method to use cached withdrawal record.

--- Block/Order/WithdrawalButton.php
+++ Block/Order/WithdrawalButton.php
@@ -6,12 +6,16 @@ namespace Zwernemann\Withdrawal\Block\Order;
 use Magento\Framework\View\Element\Template;
 use Magento\Framework\View\Element\Template\Context;
 use Zwernemann\Withdrawal\Helper\Config;
+use Zwernemann\Withdrawal\Model\Withdrawal;
 use Zwernemann\Withdrawal\Model\WithdrawalRepository;
 
 class WithdrawalButton extends Template
 {
     private $config;
     private $withdrawalRepository;
+
+    /** @var array<int, Withdrawal|null> */
+    private $withdrawalCache = [];
 
     public function __construct(
         Context $context,
@@ -41,6 +45,19 @@ class WithdrawalButton extends Template
 
     public function hasWithdrawal(int $orderId): bool
     {
-        return $this->withdrawalRepository->hasWithdrawal($orderId);
+        return $this->getWithdrawal($orderId) !== null;
+    }
+
+    /**
+     * Returns the withdrawal record for the given order, or null if none.
+     * Result is cached per order so repeated calls in a template row do not
+     * trigger additional queries.
+     */
+    public function getWithdrawal(int $orderId): ?Withdrawal
+    {
+        if (!array_key_exists($orderId, $this->withdrawalCache)) {
+            $this->withdrawalCache[$orderId] = $this->withdrawalRepository->getByOrderId($orderId);
+        }
+        return $this->withdrawalCache[$orderId];
     }
 }
